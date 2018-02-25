553bd2f5390dd6f4a363418ada774b1abc37041d
LimbolRain <limbolrain@gmail.com>
2018-02-25 19:37:12 +0800

    [ Member ] 加了点注释

--- app/src/main/java/top/tented/util/Member.kt
+++ app/src/main/java/top/tented/util/Member.kt
@@ -12,9 +12,27 @@ class Member(val group : Group, val id : Long, val name : String?) {
         apply()
     }
 
+    /**
+     * 禁言
+     * @param time 禁言时长(分钟)
+     */
     fun shutUp(time : Int) = sendWith(PluginMsg.Type.MemberShutUp) { value = time * 60 }
+
+    /**
+     * 踢人
+     */
     fun remove() = sendWith(PluginMsg.Type.RemoveMember)
+
+    /**
+     * 重命名
+     * @param newName 新名字
+     */
     fun rename(newName : String) = sendWith(PluginMsg.Type.Rename) { title = newName }
+
+    /**
+     * 点赞
+     * @param times 点赞次数, 必须在1~10之间才有效
+     */
     fun favourite(times : Int) = sendWith(PluginMsg.Type.Favourite) { if (times in 1..10) value = times }
 
     override fun equals(other : Any?) = (other as? Member)?.hashCode() == hashCode()
