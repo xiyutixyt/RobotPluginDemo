10b035d2f2f10316fdc35eff3495f5a389c94842
LimbolRain <limbolrain@gmail.com>
2018-02-25 20:58:33 +0800

    [ Config ] 新增Path工具文件, 可以用Config.extraConfig来获取数据目录下的文件啦

---
+++ app/src/main/java/top/tented/util/Path.kt
@@ -0,0 +1,15 @@
+package top.tented.util
+
+import top.tented.file.Config
+
+/**
+ * Path
+ * Created by hoshino on 18-2-25 下午8:37.
+ */
+
+val rootPath = android.os.Environment.getExternalStorageDirectory().toString() + "/matc/data/"
+
+/**
+ * 获取rootPath下的文件Config对象
+ */
+fun Config.Companion.extraConfig( extra : String ) = Config(rootPath + extra + ".properties")
