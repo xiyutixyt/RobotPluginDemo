d089d5214a2294cc88377abe83fb7675367444ff
LimbolRain <limbolrain@gmail.com>
2018-02-26 16:42:10 +0800

    [ Dada ] Use `when` instead `if`

--- app/src/main/java/top/tented/plugin/auary/Dada.kt
+++ app/src/main/java/top/tented/plugin/auary/Dada.kt
@@ -1,6 +1,6 @@
 package top.tented.plugin.auary
 
-import java.util.Date
+import java.util.*
 
 /**
  * Created by XYT on 2018/2/26.
@@ -8,9 +8,9 @@ import java.util.Date
 
 //所有类名都大写
 object Dada {
-
+    val currentDateTime get() = java.util.Calendar.getInstance()            //能用Calendar就别用Data
     val timePeriod: String
-        get() = when(currentDateTime.hours) {
+        get() = when(currentDateTime[Calendar.HOUR]) {
             in 0..5 -> "凌晨"
             in 6..7 -> "早上"
             in 8..11 -> "上午"
@@ -18,12 +18,10 @@ object Dada {
 
             else -> "晚上"
         }
-
-    val currentDateTime: Date get() = java.util.Calendar.getInstance().time
     fun operatePI(z: Double): Double {
         var sum = 2.0
-        var n = 1
-        var m = 3
+        var n = 1       //尽量不要用无意义的字符, 不会英文就 https://fanyi.baidu.com/
+        var m = 3       //然后如果自己看不懂就把中文意思注释在旁边
         var t = 2.0
         while (t > z) {
             t = t * n / m
