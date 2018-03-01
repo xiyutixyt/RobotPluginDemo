f3f9fec6e5af1690227b3d3fa943a2ba44accd2f
xiyutixyt <3070993723@qq.com>
2018-03-01 13:06:44 +0800

    更新sql语法执行

--- app/src/main/java/top/tented/util/auary/sql.kt
+++ app/src/main/java/top/tented/util/auary/sql.kt
@@ -8,6 +8,12 @@ import java.sql.*
 
 object sql {
 
+    // 就是这个数据库这个配置我就看懂127.0.0.1和root和那个password分别是数据库地址，账号，密码
+    // 似乎没有看到数据库名 所以帮我配置下吧
+    // 账号:plugeacu密码lbCD52ulYC
+    // 数据库地址：localhost
+    // 数据库端口：3306
+    // 数据库名：plugeacu
     @Throws(Exception::class)
     fun sql(sql: String) {
 
