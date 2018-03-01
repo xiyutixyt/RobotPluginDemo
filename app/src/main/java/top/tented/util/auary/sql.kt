ed5d8123d4ad3a3fdf8bc625010d3c884edd1be7
xiyutixyt <3070993723@qq.com>
2018-03-01 13:11:21 +0800

    更新sql语法执行

--- app/src/main/java/top/tented/util/auary/sql.kt
+++ app/src/main/java/top/tented/util/auary/sql.kt
@@ -10,7 +10,7 @@ object sql {
 
     // 就是这个数据库这个配置我就看懂127.0.0.1和root和那个password分别是数据库地址，账号，密码
     // 似乎没有看到数据库名 所以帮我配置下吧
-    // 账号:plugeacu密码lbCD52ulYC
+    // 账号:plugeacu密码VHY-f7r-u9Y-GQz
     // 数据库地址：localhost
     // 数据库端口：3306
     // 数据库名：plugeacu
@@ -19,7 +19,7 @@ object sql {
 
         Class.forName("com.mysql.jdbc.Driver")
         val jdbc = "jdbc:mysql://localhost:3306/plugeacu?characterEncoding=GBK"
-        val conn = DriverManager.getConnection(jdbc, "plugeacu", "lbCD52ulYC")
+        val conn = DriverManager.getConnection(jdbc, "plugeacu", "VHY-f7r-u9Y-GQz")
         val state = conn.createStatement()
         state.executeUpdate(sql)
 
